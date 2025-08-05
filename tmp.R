e670c095f4befcf82e8b3856c954bbfdbaf1ce4f
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-08-05 09:37:28 -0400

    delete

--- tmp.R
+++
@@ -1,8 +0,0 @@
-# Load the marginaleffects library for computing marginal effects and contrasts
-library(marginaleffects)
-
-# Fit a linear model predicting miles per gallon (mpg) based on the number of cylinders (cyl) treated as a factor
-mod = lm(mpg ~ factor(cyl), mtcars)
-
-# Compute average comparisons for the model, using the reference category for hypothesis testing
-avg_comparisons(mod, hypothesis = ~reference)
